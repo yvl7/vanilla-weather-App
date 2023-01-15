6b5c14b7afd93537fd1d0191354777cac10bd10e
yvl7 <106492698+yvl7@users.noreply.github.com>
2023-01-16 01:29:04 +0200

    time function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+function formatDate (timestamp) {
+let date = new Date (timestamp);
+let hours = date.getHours();
+
+if (hours < 10) {
+    hours =`0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = '0${minutes}';
+}
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours} :${minutes}`;
+}
+
+
 function ShowWeatherData (response) {
 let temperatureElement = document.querySelector("#temperature");
 temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -8,6 +26,10 @@ windElement.innerHTML = ` ${response.data.wind.speed}km/h`;
 let feelsTemperatureElement = document.querySelector("#feels");
 feelsTemperatureElement.innerHTML = Math.round(response.data.temperature.feels_like);
 console.log(response.data)
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML = response.data.city;
+let lastTimeUpdateElement = document.querySelector("#date");
+lastTimeUpdateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let units = "metric";
